a364b3c6aa863ad4af67027b5ef3551037515cea
Reuben Miller <reuben.d.miller@gmail.com>
2022-12-28 17:53:33 +0100

    feat: expose retry lib

--- Cumulocity/__init__.py
+++ Cumulocity/__init__.py
@@ -1,6 +1,7 @@
 """Cumulocity library for Robot Framework"""
 from importlib.metadata import version, PackageNotFoundError
 from .Cumulocity import Cumulocity
+from c8y_test_core import retry
 
 try:
     __version__ = version("Cumulocity")
